f053b20adea21f8bed6749a518069f219c7319a4
Zechariah Burchett <zechariah.burchett@gmail.com>
2018-10-23 10:57:54 -0400

    added markers to page

--- src/App.js
+++ src/App.js
@@ -11,23 +11,33 @@ class App extends Component {
 
   componentDidMount(){
     load_foursquare()
+      //get response from foursquare api and set state for venues
       .then((responseArray) => {
-        //console.log(responseArray.response.groups[0].items)
         this.setState({
         venues: responseArray.response.groups[0].items
       })
-    })
-    load_google_maps()
-    .then(google =>{
-      this.initMap()
+      //after we get response from foursquare get the google map
+      load_google_maps()
+      .then(google =>{
+        //after we get the google maps api connected create the map with markers
+        this.initMap()
+      })
     })
   }
 
+//this creates the map on the page
  initMap = () => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+      center: {lat: 39.758949, lng: -84.191605},
+      zoom: 12
     });
+    //this will create the markers on the page for each venue
+    this.state.venues.map(venue => {
+      let marker = new window.google.maps.Marker({
+        position: {lat: venue.venue.location.lat, lng: venue.venue.location.lng},
+        map: map
+      })
+    })
  }
 
   render() {
