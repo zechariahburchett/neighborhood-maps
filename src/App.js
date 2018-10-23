436962ad2c9478d17daf266623d25c12c0456478
Zechariah Burchett <zechariah.burchett@gmail.com>
2018-10-23 10:36:23 -0400

    connect app to foursquare api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
-import Map from './components/Map.js'
-import { load_google_maps } from './utils.js'
+import Map from './components/Map.js';
+import { load_google_maps, load_foursquare } from './utils.js';
 
 class App extends Component {
-    componentDidMount() {
-      load_google_maps()
-      .then(google => {
-         const map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 39.758949, lng: -84.191605},
-            zoom: 8
-          });
+
+  state = {
+    venues: []
+  }
+
+  componentDidMount(){
+    load_foursquare()
+      .then((responseArray) => {
+        //console.log(responseArray.response.groups[0].items)
+        this.setState({
+        venues: responseArray.response.groups[0].items
       })
-    }
+    })
+    load_google_maps()
+    .then(google =>{
+      this.initMap()
+    })
+  }
+
+ initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+ }
 
   render() {
     return (
+      <main>
         <Map />
+      </main>
     )
   }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
-//-----------------------------------------------------------------------
-//function borrowed from Ryan Waite - project coach from live project help
-export function load_google_maps() {
+/*Code borrowed from Ryan Waite - Udacity Project Coach
+  YouTube video https://www.youtube.com/watch?v=5J6fs_BlVC0&t=1259s
+  GitHub source https://github.com/ryanwaite28/script-store/blob/master/js/react_resolve_google_maps.js
+*/
+export function load_google_maps () {
   return new Promise(function(resolve, reject) {
     // define the global callback that will run when google maps is loaded
     window.resolveGoogleMapsPromise = function() {
@@ -11,10 +13,21 @@ export function load_google_maps() {
     }
     // Now, Load the Google Maps API
     const script = document.createElement("script");
-    const API_KEY = 'AIzaSyCJa1XV8XdrGyoLJFril79j7n3YryFoAro';
+    const API_KEY = 'AIzaSyDPymC8Nt9d0RojKQI1wLxuSMXjnFBBAg4';
     script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${API_KEY}&callback=resolveGoogleMapsPromise`;
     script.async = true;
     document.body.appendChild(script);
   });
 }
-//-----------------------------------------------------------------------
+//End borrowed code
+
+//load foursquare api
+export function load_foursquare () {
+  return fetch('https://api.foursquare.com/v2/venues/explore?near=Dayton&client_id=HFW1HIKPJVYEIJ0TE4PPGCXF4N32WMFVHY5J1V2DKGQZXWVV&client_secret=QABWCB4XCHBTLZSJ5ZO1IZDXSMNENTVSQDWZIC1GG2HTXDE0&v=20182507&query=coffee')
+    .then(response => {
+        return response.json()
+    })
+    .catch(error => {
+      console.log("ERROR! " + error)
+    })
+  }
