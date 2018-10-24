3884a5bd10d25117379784f4f349a430f25f1dff
Zechariah Burchett <zechariah.burchett@gmail.com>
2018-10-23 22:45:14 -0400

    created sidebar and input for sidebar

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import { load_google_maps, load_foursquare } from './utils.js';
 class App extends Component {
 
   state = {
-    venues: []
+    venues: [],
+    query: '',
+    markers: []
   }
 
   componentDidMount(){
     load_foursquare()
       //get response from foursquare api and set state for venues
       .then((responseArray) => {
-        console.log (responseArray)
+        //console.log (responseArray)
         this.setState({
         venues: responseArray.response.groups[0].items
         })
@@ -30,7 +32,7 @@ class App extends Component {
  initMap = () => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 39.758949, lng: -84.191605},
-      zoom: 14
+      zoom: 12
     });
     //create one info window on page
     let infowindow = new window.google.maps.InfoWindow()
@@ -38,16 +40,18 @@ class App extends Component {
     //create the markers and info windows on the page for each venue
     this.state.venues.map(venue => {
 
-      //get data for the info window for venue
+      //get data for the info window for the venue
       let contentString = ("<p>" + "<b>"+'Name: '+"</b>" + venue.venue.name + "<br />" +
                      "<b>" + 'Address: ' + "</b>" + venue.venue.location.address + ' ' + venue.venue.location.city +
                      ', ' + venue.venue.location.state + ' ' + venue.venue.location.postalCode + "<p />")
-      //CREATE THE MARKERS
+      //create the markers
       let marker = new window.google.maps.Marker({
         position: {lat: venue.venue.location.lat, lng: venue.venue.location.lng},
         map: map,
-        animation: window.google.maps.Animation.DROP
+        animation: window.google.maps.Animation.DROP,
+        name: venue.venue.name
       })
+      this.state.markers.push(marker)
       //add animation to a marker when it's clicked
       marker.addListener('click', () => {
         if (marker.getAnimation() !== null) {
@@ -63,13 +67,35 @@ class App extends Component {
           infowindow.open(map, marker);
         }) // end addListener for Animation
 
-      }) //end CREATE MARKERS
+      }) //end create the markers
     } //end initMap
+
+
+    filterVenues = (query) => {
+       this.setState({query: query});
+       // console.log (this.state.query)
+       //console.log (this.state.markers)
+       this.state.markers.forEach(marker =>{
+       if (marker.name.toLowerCase().includes(query.toLowerCase()) == true){
+          marker.setVisible(true)
+        } else {
+            marker.setVisible(false);
+          }
+       })
+     }
 
   render() {
     return (
       <main>
         <Map />
+        <div id="sidebar">
+          <input
+              type="text"
+              placeholder="Filter restaurants..."
+              value = {this.state.query}
+              onChange = {(event) => this.filterVenues(event.target.value)}
+            />
+        </div>
       </main>
     )
   } // end render
